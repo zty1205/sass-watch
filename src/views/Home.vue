0738b4c68cc59412c876637f65bce9e5a147e543
zty <zhangtengyue@qbb6.com>
2019-03-12 19:12:48 +0800

    all test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="home">
-    hello
-    <div class="box">123124</div>
-    <div class="parent">parent
-      <div class="child">child</div>
+    Name: {{ desc }}
+
+    <div>
+      <button @click="onAdd">添加</button>
+      <button style="margin-left: 10px;" @click="onSet">设置</button>
+    </div>
+
+    <div v-for="val in arr" :key="val"> {{ val }}</div>
+    <div>
+      <button @click="onArrAdd1">添加index</button>
+      <button style="margin-left: 10px;" @click="onArrAdd2">添加push</button>
+      <button style="margin-left: 10px;" @click="onArrSet1">设置</button>
     </div>
   </div>
 </template>
@@ -14,10 +22,43 @@
 
 export default {
   name: 'home',
+  data() {
+    return {
+      n1: "hello ",
+      num: 1,
+      n2: " zty",
+      arr: [1,2,3]
+    }
+  },
+  computed: {
+    desc() {
+      return this.n1 + this.num + this.n2
+    }
+  },
+  methods: {
+    onAdd() {
+      this.num++
+    },
+    onSet() {
+      this.n2 = " vue"
+    },
+    onArrAdd1() {
+      this.arr[3] = 4
+    },
+    onArrAdd2() {
+       this.arr.push(4)
+    },
+    onArrSet1() {
+      this.arr[1] = 222
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-@import url("../assets/scss/common");
+  .home {
+    margin-top: 100px;
+    padding-top: 20px;
+  }
 </style>
 
